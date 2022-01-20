b6aa7f5cda048798193d2ddfa6ea8fbc985a4272
RITAM-MALLICK <87016544+RITAM-MALLICK@users.noreply.github.com>
2022-01-20 20:23:23 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -119,3 +119,35 @@ def chat():
 
 
 chat()
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+       
+        else:
+           inp = message.content
+           result = model.predict([bag_of_words(inp, words)])[0]
+           result_index = np.argmax(result)
+           tag = labels[result_index]
+           
+           if result[result_index] > 0.7:
+               for tg in data["intents"]:
+                   if tg['tag'] == tag:
+                       responses = tg['responses']
+                
+               bot_response=random.choice(responses)
+               await message.channel.send(bot_response.format(message))
+           else:
+               await message.channel.send("I didnt get that. Can you explain or try again.".format(message))
+
+client = MyClient()
+client.run('OTMzNjg3Mjc4MjU2MTI4MDEw.YelKCg._z3AD53iTcWwuPiCn4Ok5MHTYMc')
